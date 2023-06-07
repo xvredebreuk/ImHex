bab4a3773593382febe9751d9abfc33318b63a61
WerWolv <werwolv98@gmail.com>
2023-06-07 23:52:30 +0200

    feat: Added basic implementation for crash reporting

--- main/source/crash_handlers.cpp
+++ main/source/crash_handlers.cpp
@@ -1,7 +1,10 @@
 #include <hex/api/project_file_manager.hpp>
+#include <hex/api_urls.hpp>
+
 #include <hex/helpers/logger.hpp>
 #include <hex/helpers/fs.hpp>
 #include <hex/helpers/stacktrace.hpp>
+#include <hex/helpers/http_requests.hpp>
 
 #include <wolv/io/fs.hpp>
 #include <wolv/utils/string.hpp>
@@ -15,6 +18,10 @@
 #include <exception>
 #include <csignal>
 
+#if defined (OS_MACOS)
+    #include <sys/utsname.h>
+#endif
+
 namespace hex::crash {
 
     constexpr static auto CrashBackupFileName = "crash_backup.hexproj";
@@ -27,7 +34,7 @@ namespace hex::crash {
     }
 
     // function that decides what should happen on a crash
-    // (either sending a message or saving a crash file, depending on when the crash occured)
+    // (either sending a message or saving a crash file, depending on when the crash occurred)
     static std::function<void(const std::string&)> crashCallback = sendNativeMessage;
 
     static void saveCrashFile(const std::string& message) {
@@ -44,10 +51,33 @@ namespace hex::crash {
                 file.writeString(crashData.dump(4));
                 file.close();
                 log::info("Wrote crash.json file to {}", wolv::util::toUTF8String(file.getPath()));
+
                 return;
             }
         }
         log::warn("Could not write crash.json file !");
+    }
+
+    static void uploadCrashLog(const std::fs::path &path) {
+        wolv::io::File logFile(path, wolv::io::File::Mode::Read);
+        if (!logFile.isValid())
+            return;
+
+        // Read current log file data
+        auto data = logFile.readString();
+
+        // Anonymize the log file
+        {
+            for (u32 pathType = 0; pathType < u32(fs::ImHexPath::END); pathType++) {
+                for (auto &folder : fs::getDefaultPaths(static_cast<fs::ImHexPath>(pathType))) {
+                    auto parent = wolv::util::toUTF8String(folder.parent_path());
+                    data = wolv::util::replaceStrings(data, parent, "<*****>");
+                }
+            }
+        }
+
+        HttpRequest request("POST", ImHexApiURL + std::string("/crash_upload"));
+        request.uploadFile(std::vector<u8>(data.begin(), data.end()), "file", path.filename()).wait();
     }
 
 
@@ -78,6 +108,8 @@ namespace hex::crash {
             else
                 log::fatal("  ({}:{}) | {}",  stackFrame.file, stackFrame.line, stackFrame.function);
         }
+
+        uploadCrashLog(hex::log::getFile().getPath());
     
         // Trigger a breakpoint if we're in a debug build or raise the signal again for the default handler to handle it
         #if defined(DEBUG)
